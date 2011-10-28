5c44c855fd84cda7ad93853903d80e4ac0afe007
Pat O'Keefe <patokeefe1@gmail.com>
2011-10-28 10:14:48 -0400

    Added license header to the source files.

--- CoreMotionLogger/DataLogger.m
+++ CoreMotionLogger/DataLogger.m
@@ -3,7 +3,24 @@
 //  CoreMotionLogger
 //
 //  Created by Patrick O'Keefe on 10/27/11.
-//  Copyright (c) 2011 Patrick O'Keefe. All rights reserved.
+//  Copyright (c) 2011 Patrick O'Keefe.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this
+//  software and associated documentation files (the "Software"), to deal in the Software
+//  without restriction, including without limitation the rights to use, copy, modify,
+//  merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies
+//  or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+//  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+//  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import "DataLogger.h"
@@ -31,13 +48,13 @@
         // string at the same time.
         _deviceMotionQueue = [[NSOperationQueue alloc] init];
         [_deviceMotionQueue setMaxConcurrentOperationCount:1];
-        
+
         _accelQueue = [[NSOperationQueue alloc] init];
         [_accelQueue setMaxConcurrentOperationCount:1];
 
         _gyroQueue = [[NSOperationQueue alloc] init];
         [_gyroQueue setMaxConcurrentOperationCount:1];
-        
+
         _logAttitudeData = false;
         _logGravityData = false;
         _logMagneticFieldData = false;
@@ -45,7 +62,7 @@
         _logUserAccelerationData = false;
         _logRawGyroscopeData = false;
         _logRawAccelerometerData = false;
-        
+
         _attitudeString = [[NSString alloc] init];
         _gravityString = [[NSString alloc] init];
         _magneticFieldString = [[NSString alloc] init];
@@ -60,22 +77,22 @@
 }
 
 - (void) startLoggingMotionData {
-    
+
     NSLog(@"Starting to log motion data.");
 
     CMDeviceMotionHandler motionHandler = ^(CMDeviceMotion *motion, NSError *error) {
         [self processMotion:motion withError:error];
     };
-    
+
     CMGyroHandler gyroHandler = ^(CMGyroData *gyroData, NSError *error) {
         [self processGyro:gyroData withError:error];
     };
-    
+
     CMAccelerometerHandler accelHandler = ^(CMAccelerometerData *accelerometerData, NSError *error) {
         [self processAccel:accelerometerData withError:error];
     };
 
-    
+
     if (_logAttitudeData || _logGravityData || _logMagneticFieldData || _logRotationRateData || _logUserAccelerationData ) {
         [_motionManager startDeviceMotionUpdatesToQueue:_deviceMotionQueue withHandler:motionHandler];
     }
@@ -91,89 +108,89 @@
 }
 
 - (void) stopLoggingMotionData {
-    
+
     NSLog(@"Stopping data logging.");
 
     [_motionManager stopDeviceMotionUpdates];
     [_deviceMotionQueue waitUntilAllOperationsAreFinished];
-    
+
     [_motionManager stopAccelerometerUpdates];
     [_accelQueue waitUntilAllOperationsAreFinished];
-    
+
     [_motionManager stopGyroUpdates];
     [_gyroQueue waitUntilAllOperationsAreFinished];
 
     // Save all of the data!
     [self writeDataToDisk];
-    
+
 }
-    
+
 - (void) processAccel:(CMAccelerometerData*)accelData withError:(NSError*)error {
-    
+
     if (_logRawAccelerometerData) {
         _rawAccelerometerString = [_rawAccelerometerString stringByAppendingFormat:@"%f,%f,%f,%f\n", accelData.timestamp,
-                                   accelData.acceleration.x,
-                                   accelData.acceleration.y,
-                                   accelData.acceleration.z,
-                                   nil];
+                                                           accelData.acceleration.x,
+                                                           accelData.acceleration.y,
+                                                           accelData.acceleration.z,
+                                                           nil];
     }
 }
-    
+
 - (void) processGyro:(CMGyroData*)gyroData withError:(NSError*)error {
-    
+
     if (_logRawGyroscopeData) {
         _rawGyroscopeString = [_rawGyroscopeString stringByAppendingFormat:@"%f,%f,%f,%f\n", gyroData.timestamp,
-                               gyroData.rotationRate.x,
-                               gyroData.rotationRate.y,
-                               gyroData.rotationRate.z,
-                               nil];
+                                                   gyroData.rotationRate.x,
+                                                   gyroData.rotationRate.y,
+                                                   gyroData.rotationRate.z,
+                                                   nil];
     }
 }
 
 - (void) processMotion:(CMDeviceMotion*)motion withError:(NSError*)error {
 
-//    NSLog(@"Processing motion with motion pointer %p",motion);
-//    NSLog(@"Curr accel string %@",_userAccelerationString);
-    
+    //    NSLog(@"Processing motion with motion pointer %p",motion);
+    //    NSLog(@"Curr accel string %@",_userAccelerationString);
+
     if (_logAttitudeData) {
         _attitudeString = [_attitudeString stringByAppendingFormat:@"%f,%f,%f,%f\n", motion.timestamp,
-                           motion.attitude.pitch,
-                           motion.attitude.yaw,
-                           motion.attitude.roll,
-                           nil];
+                                           motion.attitude.roll,
+                                           motion.attitude.pitch,
+                                           motion.attitude.yaw,
+                                           nil];
     }
-    
+
     if (_logGravityData) {
         _gravityString = [_gravityString stringByAppendingFormat:@"%f,%f,%f,%f\n", motion.timestamp,
-                          motion.gravity.x,
-                          motion.gravity.y,
-                          motion.gravity.z,
-                          nil];
+                                         motion.gravity.x,
+                                         motion.gravity.y,
+                                         motion.gravity.z,
+                                         nil];
     }
-    
+
     if (_logMagneticFieldData) {
         _magneticFieldString = [_magneticFieldString stringByAppendingFormat:@"%f,%f,%f,%f,%d\n", motion.timestamp,
-                                motion.magneticField.field.x,
-                                motion.magneticField.field.y,
-                                motion.magneticField.field.z,
-                                (int)motion.magneticField.accuracy,
-                                nil];
+                                                     motion.magneticField.field.x,
+                                                     motion.magneticField.field.y,
+                                                     motion.magneticField.field.z,
+                                                     (int)motion.magneticField.accuracy,
+                                                     nil];
     }
-    
+
     if (_logRotationRateData) {
         _rotationRateString = [_rotationRateString stringByAppendingFormat:@"%f,%f,%f,%f\n", motion.timestamp,
-                               motion.rotationRate.x,
-                               motion.rotationRate.y,
-                               motion.rotationRate.z,
-                               nil];
+                                                   motion.rotationRate.x,
+                                                   motion.rotationRate.y,
+                                                   motion.rotationRate.z,
+                                                   nil];
     }
-    
+
     if (_logUserAccelerationData) {
         _userAccelerationString = [_userAccelerationString stringByAppendingFormat:@"%f,%f,%f,%f\n", motion.timestamp,
-                                   motion.userAcceleration.x,
-                                   motion.userAcceleration.y,
-                                   motion.userAcceleration.z,
-                                   nil];
+                                                           motion.userAcceleration.x,
+                                                           motion.userAcceleration.y,
+                                                           motion.userAcceleration.z,
+                                                           nil];
     }
 
 }
@@ -226,7 +243,7 @@
     // Nobody can stand forward slashes
     dateString = [dateString stringByReplacingOccurrencesOfString:@"/" withString:@"_"];
 
-    
+
     if (_logAttitudeData) {
         NSString *fullPath = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"attitude_%@.txt", dateString, nil]];
 
